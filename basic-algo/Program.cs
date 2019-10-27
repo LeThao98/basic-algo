ce2741b5e11c8f2d45604f0cbdf277fa26c9618d
LeThao98 <ledinhthao131098@gmail.com>
2019-10-27 14:35:39 +0700

    001

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,13 +7,29 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
+        Write a C# Sharp program to compute the sum of the two given integer values.
+        If the two values are the same, then return triple their sum. Go to the editor
 
+        Expected Output:
+            3
+            5
+            12
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine(act(1, 2));
+            Console.WriteLine(act(2, 3));
+            Console.WriteLine(act(2, 2));
+
+            Console.ReadKey();
+        }
+
+        private static int act(int a, int b)
+        {
+            return a == b ? 3 * (a + b) : a + b;
         }
     }
 }
