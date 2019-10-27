4565349d5e400a7443e96a4eabed74a2d3c9eed5
LeThao98 <ledinhthao131098@gmail.com>
2019-10-27 16:09:07 +0700

    008

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,23 +3,25 @@
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to exchange the first and last characters in a given string and return the new string.
+        Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string.
+        If the given string length is less than 2 return the original string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(act("Python"));
-            Console.WriteLine(act("x"));
+            Console.WriteLine(act("C Sharp"));
+            Console.WriteLine(act("JS"));
+            Console.WriteLine(act("a"));
 
             Console.ReadKey();
         }
 
         private static string act(string str)
         {
-            return str.Length > 1 ?
-                str.Substring(str.Length - 1, 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1)
+            return str.Length >= 2 ?
+                str.Substring(0, 2) + str.Substring(0, 2) + str.Substring(0, 2) + str.Substring(0, 2)
                 :
                 str;
         }
