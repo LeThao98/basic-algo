f14c42f6d8eb9899ff468bc2815aefb0d41c855f
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 22:02:02 +0700

    017

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,29 +3,24 @@
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check whether two given integer values are in the range 20..50 inclusive.
-        Return true if 1 or other is in the said range otherwise false.
+         Write a C# Sharp program to check if a string 'yt' appears at index 1 in a given string.
+         If it appears return a string without 'yt' otherwise return the original string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(20, 84));
-            Console.WriteLine(test(14, 50));
-            Console.WriteLine(test(11, 45));
-            Console.WriteLine(test(25, 40));
+            Console.WriteLine(test("Python"));
+            Console.WriteLine(test("ytade"));
+            Console.WriteLine(test("jsues"));
 
             Console.ReadKey();
         }
 
-        private static bool test(int x, int y)
+        private static string test(string str)
         {
-            return !(
-                        (x >= 20 && x <= 50)
-                            &
-                        (y >= 20 && y <= 50)
-                    );
+            return str.Substring(1).StartsWith("yt") ? str.Substring(0, 1) + str.Substring(3) : str;
         }
     }
 }
