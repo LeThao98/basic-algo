5d03263c32a97ad71e994d01e07c2064701ee47d
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 23:21:44 +0700

    025

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,25 +3,28 @@
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to convert the last 3 characters of a given string in upper case. If the length
-         of the string has less than 3 then uppercase all the characters.
+         Write a C# Sharp program to create a new string which is n (non-negative integer ) copies of a given string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Python"));
-            Console.WriteLine(test("Javascript"));
-            Console.WriteLine(test("js"));
-            Console.WriteLine(test("PHP"));
+            Console.WriteLine(test("JS", 2));
+            Console.WriteLine(test("JS", 3));
+            Console.WriteLine(test("JS", 1));
 
             Console.ReadKey();
         }
 
-        private static string test(string str)
+        private static string test(string str, int n)
         {
-            return str.Length < 3 ? str.ToUpper() : str.Substring(0, str.Length - 3) + str.Substring(str.Length - 3).ToUpper();
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                result += str;
+            }
+            return result;
         }
     }
 }
