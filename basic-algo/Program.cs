3b9c98c806809d8f4694f98341ac2f688365c7fb
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 23:39:05 +0700

    027

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,40 +3,33 @@
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to create a new string which is n (non-negative integer )copies of the the first 3 characters of a given string.
-         If the length of the given string is less than 3 then return n copies of the string.
+         Write a C# Sharp program to count the string "aa" in a given string and assume "aaa" contains two "aa".
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Python", 2));
-            Console.WriteLine(test("Python", 3));
-            Console.WriteLine(test("JS", 3));
+            Console.WriteLine(test("bbaaccaag"));
+            Console.WriteLine(test("jjkiaaasew"));
+            Console.WriteLine(test("JSaaakoiaa"));
 
             Console.ReadKey();
         }
 
-        private static string test(string str, int n)
+        private static int test(string str)
         {
-            string result = string.Empty;
-            if (str.Length < 3)
+            int count = 0;
+            for (int i = 1; i < str.Length - 1; i++)
+            //{
+            //    if (str[i] == 'a' && str[i - 1] == 'a')
+            //        count++;
+            //}
             {
-                for (int i = 0; i < n; i++)
-                {
-                    result += str;
-                }
+                if (str.Substring(i, 2) == "aa")
+                    count++;
             }
-            else
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    result += str.Substring(0, 3);
-                }
-                result += str.Substring(3);
-            }
-            return result;
+            return count;
         }
     }
 }
