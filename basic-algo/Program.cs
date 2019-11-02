e45ef6f93143482d7530deb2799c6e5163139744
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 20:05:34 +0700

    012

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,24 +3,29 @@
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new string taking the first 3 characters of a given string and
-       return the string with the 3 characters added at both the front and back.
-       If the given string length is less than 3, use whatever characters are there.
+       Write a C# program to that takes a number as input and display it four times in a row (separated by blank spaces),
+       and then four times in the next row, with no separation. You should do it two times: Use Console. Write and then use {0}
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(act("Python"));
-            Console.WriteLine(act("xy"));
+            act();
 
             Console.ReadKey();
         }
 
-        private static string act(string x)
+        private static void act()
         {
-            return x.Length < 3 ? x + x + x : x.Substring(0, 3) + x + x.Substring(0, 3);
+            int x;
+            Console.WriteLine("Enter a digit: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} {0} {0} {0}", x);
+            Console.WriteLine("{0}{0}{0}{0}", x);
+            Console.WriteLine("{0} {0} {0} {0}", x);
+            Console.WriteLine("{0}{0}{0}{0}", x);
         }
     }
 }
