9cbe1d7626331a1d8a40166edbb6a5103967e632
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 22:41:02 +0700

    022

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,33 +3,29 @@
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to find the larger value from two positive integer values that is in the range 20..30 inclusive,
-         or return 0 if neither is in that range.
+         Write a C# Sharp program to check if a given string contains between 2 and 4 'z' character.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(78, 95));
-            Console.WriteLine(test(20, 30));
-            Console.WriteLine(test(21, 25));
-            Console.WriteLine(test(28, 28));
+            Console.WriteLine(test("frizz"));
+            Console.WriteLine(test("zane"));
+            Console.WriteLine(test("Zazz"));
+            Console.WriteLine(test("false"));
 
             Console.ReadKey();
         }
 
-        private static int test(int x, int y)
+        private static bool test(string str)
         {
-            if ((x >= 20 && x <= 30) && (y >= 20 && y <= 30))
+            int count = 0;
+            foreach (char item in str)
             {
-                return Math.Max(x, y);
+                if (item == 'z') count++;
             }
-            else if (x >= 20 && x <= 30)
-                return x;
-            else if (y >= 20 && y <= 30)
-                return y;
-            else return 0;
+            return count > 1 && count < 4;
         }
     }
 }
