b69061755873a499700144158c989bf846020e04
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 23:32:24 +0700

    026

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,26 +3,38 @@
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to create a new string which is n (non-negative integer ) copies of a given string.
+         Write a C# Sharp program to create a new string which is n (non-negative integer )copies of the the first 3 characters of a given string.
+         If the length of the given string is less than 3 then return n copies of the string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("JS", 2));
+            Console.WriteLine(test("Python", 2));
+            Console.WriteLine(test("Python", 3));
             Console.WriteLine(test("JS", 3));
-            Console.WriteLine(test("JS", 1));
 
             Console.ReadKey();
         }
 
         private static string test(string str, int n)
         {
-            string result = "";
-            for (int i = 0; i < n; i++)
+            string result = string.Empty;
+            if (str.Length < 3)
             {
-                result += str;
+                for (int i = 0; i < n; i++)
+                {
+                    result += str;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    result += str.Substring(0, 3);
+                }
+                result += str.Substring(3);
             }
             return result;
         }
