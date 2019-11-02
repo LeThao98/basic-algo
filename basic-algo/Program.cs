a9e5c8eb49d7a131b97d9106d2f06a01dc931430
LeThao98 <ledinhthao131098@gmail.com>
2019-11-02 21:52:23 +0700

    015

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,23 +3,24 @@
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to check two given integers whether either of them is in the range 100..200 inclusive.
+       Write a C# Sharp program to check whether three given integer values are in the range 20..50 inclusive. Return true if 1 or more of them are in the said range otherwise false.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(100, 199));
-            Console.WriteLine(test(250, 300));
-            Console.WriteLine(test(105, 190));
+            Console.WriteLine(test(11, 20, 12));
+            Console.WriteLine(test(30, 30, 17));
+            Console.WriteLine(test(25, 35, 50));
+            Console.WriteLine(test(15, 12, 8));
 
             Console.ReadKey();
         }
 
-        private static bool test(int x, int y)
+        private static bool test(int x, int y, int z)
         {
-            return x >= 100 && x <= 200 | y >= 100 && y <= 200;
+            return (x >= 20 && x <= 50) | (y >= 20 && y <= 50) | (z >= 20 && z <= 50);
         }
     }
 }
