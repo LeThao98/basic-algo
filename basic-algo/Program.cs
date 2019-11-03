a7b48b0245b6657fbc0e8f428619a1f77592310d
LeThao98 <ledinhthao131098@gmail.com>
2019-11-03 20:25:16 +0700

    030

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,16 +3,16 @@
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to create a new string made of every other character starting with the first from a given string.
+         Write a C# Sharp program to create a string like  "aababcabcd" from a given string "abcd".
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Python"));
-            Console.WriteLine(test("PHP"));
-            Console.WriteLine(test("JS"));
+            Console.WriteLine(test("abcd"));
+            Console.WriteLine(test("abc"));
+            Console.WriteLine(test("a"));
 
             Console.ReadKey();
         }
@@ -22,10 +22,7 @@ namespace basic_algo
             string result = string.Empty;
             for (int i = 0; i < s.Length; i++)
             {
-                if (i % 2 == 0)
-                {
-                    result += s[i];
-                }
+                result += s.Substring(0, i + 1);
             }
             return result;
         }
