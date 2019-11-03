9d09a0993590ab3a1b6181274080b87eacc537ed
LeThao98 <ledinhthao131098@gmail.com>
2019-11-03 21:07:17 +0700

    032

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -1,34 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to count a substring of length 2 appears in a given string and
-         also as the last 2 characters of the string. Do not count the end substring.
+         Write a C# Sharp program to check a specified number is preset in a given array of integers.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("abcdsab"));
-            Console.WriteLine(test("abcdabab"));
-            Console.WriteLine(test("abcabdabab"));
-            Console.WriteLine(test("abcabd"));
-
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Console.WriteLine(test(new[] { 1, 2, 5, 4, 3 }, 3));
+            Console.WriteLine(test(new[] { 1, 2, 2, 3 }, 2));
+            Console.WriteLine(test(new[] { 1, 2, 2, 3 }, 9));
+            long elapseMs = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine(elapseMs);
             Console.ReadKey();
         }
 
-        private static int test(string s)
+        private static bool test(int[] a, int x)
         {
-            int result = 0;
-            string last2Chars = s.Substring(s.Length - 2);
-            for (int i = 0; i < s.Length - 3; i++)
-            {
-                if (s.Substring(i, 2).Equals(last2Chars))
-                    result++;
-            }
-            return result;
+            return a.Contains(x);
         }
     }
 }
