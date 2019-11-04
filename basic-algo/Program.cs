d38aad957b3eca13a35665fb705c8ebc155e2a6c
LeThao98 <ledinhthao131098@gmail.com>
2019-11-04 22:28:53 +0700

    042

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,23 +5,24 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program that accept two integers and return true if either one is 5 or their sum or difference is 5.
+         Write a C# Sharp program to test if a given non-negative number is a multiple of 13 or it is one more than a multiple of 13.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(5, 4));
-            Console.WriteLine(test(4, 3));
-            Console.WriteLine(test(1, 4));
+            Console.WriteLine(test(13));
+            Console.WriteLine(test(14));
+            Console.WriteLine(test(27));
+            Console.WriteLine(test(41));
 
             Console.ReadKey();
         }
 
-        private static bool test(int a, int b)
+        private static bool test(int a)
         {
-            return a == 5 || b == 5 || a + b == 5 ? true : false;
+            return a % 13 == 0 || a % 13 == 1;
         }
     }
 }
