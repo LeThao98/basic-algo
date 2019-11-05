52a98ae8d8c24e3e9716b98fbe5ba23cfe7c34f1
LeThao98 <ledinhthao131098@gmail.com>
2019-11-05 09:15:21 +0700

    047

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,25 +5,23 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to compute the sum of the two given integers.
-         If one of the given integer value is in the range 10..20 inclusive return 18.
+         Write a C# Sharp program to check if it is possible to add two integers to get the third integer from three given integers.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(3, 7));
-            Console.WriteLine(test(10, 11));
-            Console.WriteLine(test(10, 20));
-            Console.WriteLine(test(21, 220));
+            Console.WriteLine(test(1, 2, 3));
+            Console.WriteLine(test(4, 5, 6));
+            Console.WriteLine(test(-1, 1, 0));
 
             Console.ReadKey();
         }
 
-        private static int test(int x, int y)
+        private static bool test(int a, int b, int c)
         {
-            return (x >= 10 && x <= 20) || (y >= 10 && y <= 20) ? 18 : x + y;
+            return a == b + c || b == a + c || c == a + b;
         }
     }
 }
