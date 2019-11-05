f90d6b05cfe703f9e6a7f8e029125c94c72af1fd
LeThao98 <ledinhthao131098@gmail.com>
2019-11-05 09:35:44 +0700

    049

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,23 +5,26 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to check if y is greater than x, and z is greater than y from three given integers x,y,z.
+         Write a C# Sharp program to check if three given numbers are in strict increasing order,
+         such as 4 7 15, or 45, 56, 67, but not 4 ,8, 8 or 6, 6, 8.
+         However,if a fourth parameter  is true, equality is allowed, such as 6, 6, 8 or 7, 7, 7.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(1, 2, 3));
-            Console.WriteLine(test(4, 5, 6));
-            Console.WriteLine(test(-1, 1, 0));
+            Console.WriteLine(test(1, 2, 3, false));
+            Console.WriteLine(test(1, 2, 3, true));
+            Console.WriteLine(test(10, 2, 30, false));
+            Console.WriteLine(test(10, 10, 30, true));
 
             Console.ReadKey();
         }
 
-        private static bool test(int x, int y, int z)
+        public static bool test(int x, int y, int z, bool flag)
         {
-            return y > x && z > y;
+            return flag ? x <= y && y <= z : x < y && y < z;
         }
     }
 }
