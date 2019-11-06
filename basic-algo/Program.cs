ca022c27679ee11d9aed401baf80f08e6474a9d3
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 11:18:08 +0700

    071

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to create a new string without the first and last character of a given string of any length.
+         Write a C# Sharp program to create a new string using the two middle characters of a given string of even length (at least 2).
     */
 
     internal class Program
@@ -21,7 +21,7 @@ namespace basic_algo
 
         public static string test(string s)
         {
-            return s.Length >= 2 ? s.Substring(1, s.Length - 2) : string.Empty;
+            return s.Length >= 2 ? s.Substring((s.Length / 2) - 1, 2) : string.Empty;
         }
     }
 }
