46d13b8607025bcae125fe66a1071c1fb5b7a493
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 10:24:09 +0700

    054

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,24 +5,32 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to check two given integers, each in the range 10..99.
-         Return true if a digit appears in both numbers, such as the 3 in 13 and 33
+         Write a C# Sharp program to compute the sum of two given non-negative integers x and y as long as the sum has the same number of digits as x.
+         If the sum has more digits than x then return x without y.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(11, 21));
-            Console.WriteLine(test(11, 20));
+            Console.WriteLine(test(4, 5));
+            Console.WriteLine(test(7, 4));
             Console.WriteLine(test(10, 10));
 
             Console.ReadKey();
         }
 
-        public static bool test(int x, int y)
+        public static int test(int x, int y)
         {
-            return (x / 10 == y / 10 || x / 10 == y % 10) || (x % 10 == y / 10 || x % 10 == y % 10);
+            int a = x + y;
+            int b = x;
+            while (a > 0 && b > 0)
+            {
+                a /= 10;
+                b /= 10;
+            }
+            return a > b ? x : x + y;
+            //return (x + y).ToString().Length > x.ToString().Length ? x : x + y;
         }
     }
 }
