1c9d3c2a1ceb100f8609a29c394ae7a16fc9bab3
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 10:56:18 +0700

    061

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,21 +5,21 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to create a new string using two given strings s1, s2, the format of the new string will be s1s2s2s1.
+         Write a C# Sharp program to insert a given string into middle of the another given string of length 4.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Hi", "Hello"));
-            Console.WriteLine(test("whats", "app"));
+            Console.WriteLine(test("[[]]", "Hello"));
+            Console.WriteLine(test("(())", "Hi"));
             Console.ReadKey();
         }
 
         public static string test(string s1, string s2)
         {
-            return String.Format("{0}{1}{1}{0}", s1, s2);
+            return s1.Length == 4 ? s1.Substring(0, 2) + s2 + s1.Substring(2) : string.Empty;
         }
     }
 }
