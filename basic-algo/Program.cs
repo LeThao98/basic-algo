5d7391dbfd4f4473352965e2917a0a1d989af121
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 10:29:56 +0700

    056

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,7 +5,8 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to compute the sum of three given integers. If the two values are same return the third value.
+         Write a C# Sharp program to compute the sum of the three integers.
+         If one of the values is 13 then do not count it and its right towards the sum.
     */
 
     internal class Program
@@ -14,18 +15,17 @@ namespace basic_algo
         {
             Console.WriteLine(test(4, 5, 7));
             Console.WriteLine(test(7, 4, 12));
-            Console.WriteLine(test(10, 10, 12));
-            Console.WriteLine(test(12, 12, 18));
+            Console.WriteLine(test(10, 13, 12));
+            Console.WriteLine(test(13, 12, 18));
 
             Console.ReadKey();
         }
 
         public static int test(int x, int y, int z)
         {
-            if (x == y && y == z) return 0;
-            if (x == y) return z;
-            if (x == z) return y;
-            if (y == z) return x;
+            if (x == 13) return 0;
+            if (y == 13) return x;
+            if (z == 13) return x + y;
             return x + y + z;
         }
     }
