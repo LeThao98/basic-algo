6b911889ba68c2a773d39d2a38fe2613f4557c28
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 11:12:34 +0700

    068

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,22 +5,22 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to concat two given string of length atleast 1, after removing their first character.
+         Write a C# Sharp program to move the first two characters to the end of a given string of length at least two.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Hello", "Hi"));
-            Console.WriteLine(test("JS", "Python"));
+            Console.WriteLine(test("Hello"));
+            Console.WriteLine(test("JS"));
 
             Console.ReadKey();
         }
 
-        public static string test(string s1, string s2)
+        public static string test(string s)
         {
-            return s1.Substring(1) + s2.Substring(1);
+            return s.Length >= 2 ? s.Substring(2) + s.Substring(0, 2) : s;
         }
     }
 }
