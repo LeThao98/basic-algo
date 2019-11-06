52cc9341ba0fa4a2221e8e0e8b9742562a4e6bef
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 10:53:50 +0700

    060

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,24 +5,21 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to check three given integers (small, medium and large) and return true if the difference between small
-         and medium and the difference between medium and large is same.
+         Write a C# Sharp program to create a new string using two given strings s1, s2, the format of the new string will be s1s2s2s1.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(4, 5, 6));
-            Console.WriteLine(test(7, 12, 13));
-            Console.WriteLine(test(-1, 0, 1));
-
+            Console.WriteLine(test("Hi", "Hello"));
+            Console.WriteLine(test("whats", "app"));
             Console.ReadKey();
         }
 
-        public static bool test(int x, int y, int z)
+        public static string test(string s1, string s2)
         {
-            return z - y == y - x;
+            return String.Format("{0}{1}{1}{0}", s1, s2);
         }
     }
 }
