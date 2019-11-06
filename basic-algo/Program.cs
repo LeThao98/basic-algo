11a983351992932fb26177d3863cb7d2a2977f48
LeThao98 <ledinhthao131098@gmail.com>
2019-11-06 11:07:16 +0700

    065

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-         Write a C# Sharp program to create a new string of the first half of a given string of even length.
+         Write a C# Sharp program to create a new string without the first and last character of a given string of length atleast two.
     */
 
     internal class Program
@@ -14,12 +14,14 @@ namespace basic_algo
         {
             Console.WriteLine(test("Hello"));
             Console.WriteLine(test("Hi"));
+            Console.WriteLine(test("Python"));
+
             Console.ReadKey();
         }
 
         public static string test(string s)
         {
-            return s.Length % 2 == 0 ? s.Substring(0, s.Length / 2) : s.Substring(0, (s.Length - 1) / 2);
+            return s.Length >= 2 ? s.Substring(1, s.Length - 2) : s;
         }
     }
 }
