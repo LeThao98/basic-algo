3c192edb20d0df54894d79c332d20131a8e8aece
LeThao98 <ledinhthao131098@gmail.com>
2019-11-07 20:37:11 +0700

    075

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,23 +5,24 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new string of length 2 starting at the given index of a given string.
+        Write a C# Sharp program to create a new string taking 3 characters from the middle of a given string at least 3.
+        Wind, Light, Ocean, Electric, Land
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Hello", 1));
-            Console.WriteLine(test("Python", 2));
-            Console.WriteLine(test("on", 1));
+            Console.WriteLine(test("Hello"));
+            Console.WriteLine(test("Python"));
+            Console.WriteLine(test("abc"));
 
             Console.ReadKey();
         }
 
-        public static string test(string s, int n)
+        public static string test(string s)
         {
-            return n <= s.Length - 2 ? s.Substring(n, 2) : (s.Length == 1 ? s : s.Substring(0, 2));
+            return s.Length >= 3 ? s.Substring((s.Length - 1) / 2 - 1, 3) : string.Empty;
         }
     }
 }
