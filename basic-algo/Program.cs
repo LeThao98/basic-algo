d8277729daebf716d5f3cb3db712b3de51b83884
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 11:08:11 +0700

    097

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,14 +5,14 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new array containing the middle elements from the two given arrays of integers, each length 5.
+       Write a C# Sharp program to create a new array taking the first and last elements of a given array of integers and length 1 or more.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 10, 20, -30, -40, 30 }, new[] { 10, 20, 30, 40, 30 });
+            int[] item = test(new[] { 10, 20, -30, -40, 30 });
 
             Console.Write("New array: ");
 
@@ -24,9 +24,9 @@ namespace basic_algo
             Console.ReadKey();
         }
 
-        public static int[] test(int[] nums1, int[] nums2)
+        public static int[] test(int[] nums)
         {
-            return new int[] { nums1[2], nums2[2] };
+            return new int[] { nums[0], nums[nums.Length - 1] };
         }
     }
 }
