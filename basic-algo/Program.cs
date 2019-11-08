840742db6b8ce1eec2fcacaca058d6d151c33b09
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 11:15:31 +0700

    099

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,23 +5,32 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to check if a given array of integers and length 2, does not contain 15 and 20.
+       Write a C# Sharp program to create a new array of integers and length 1 or more.
+       The length of the new array will be double length of an given array and
+       all the elements are 0 except the first element which is equal to first elements of the given array.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 12, 20 }));
-            Console.WriteLine(test(new[] { 14, 15 }));
-            Console.WriteLine(test(new[] { 11, 21 }));
+            int[] item = test(new[] { 10, 20, -30, -40, 30 });
+            Console.Write("New array: ");
+            foreach (var i in item)
+            {
+                Console.Write(i.ToString() + " ");
+            }
 
             Console.ReadKey();
         }
 
         public static int[] test(int[] nums)
         {
-            return nums[0] != 15 && nums[0] != 20 && nums[1] != 15 && nums[1] != 20;
+            int elements = nums.Length * 2;
+            int[] doubleNums = new int[elements];
+
+            doubleNums[0] = nums[0];
+            return doubleNums;
         }
     }
 }
