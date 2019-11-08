d66da2092c4b9466b27375433a0e98e3459c095f
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 10:28:19 +0700

    078

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,27 +5,33 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new string taking the first character from a given string and the last character from another given string.
-       If the length of any given string is 0, use '#' as its  missing character.
+       Write a C# Sharp program to concate two given strings. If there are any double character in new string then omit one character.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Hello", "Hi"));
-            Console.WriteLine(test("Python", "PHP"));
-            Console.WriteLine(test("JS", "JS"));
-            Console.WriteLine(test("Csharp", ""));
+            Console.WriteLine(test("abc", "cat"));
+            Console.WriteLine(test("python", "php"));
+            Console.WriteLine(test("php", "php"));
 
             Console.ReadKey();
         }
 
         public static string test(string s1, string s2)
         {
-            string firstChar = s1.Length == 0 ? "#" : s1.Substring(0, 1);
-            string secondChar = s2.Length == 0 ? "#" : s2.Substring(s2.Length - 1);
-            return firstChar + secondChar;
+            if (s1.Length < 1)
+            {
+                return s2;
+            }
+
+            if (s2.Length < 1)
+            {
+                return s1;
+            }
+
+            return !s1.EndsWith(s2.Substring(0, 1)) ? s1 + s2 : s1 + s2.Substring(1);
         }
     }
 }
