e12495e68a2ce6a4dc4ed23daf2a37ab8e8835ab
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 10:20:31 +0700

    077

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,24 +5,27 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new string of length 2, using first two characters of a given string. If the given string length is less than 2 use '#' as missing characters.
+       Write a C# Sharp program to create a new string taking the first character from a given string and the last character from another given string.
+       If the length of any given string is 0, use '#' as its  missing character.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("Hello"));
-            Console.WriteLine(test("Python"));
-            Console.WriteLine(test("a"));
-            Console.WriteLine(test(""));
+            Console.WriteLine(test("Hello", "Hi"));
+            Console.WriteLine(test("Python", "PHP"));
+            Console.WriteLine(test("JS", "JS"));
+            Console.WriteLine(test("Csharp", ""));
 
             Console.ReadKey();
         }
 
-        public static string test(string s)
+        public static string test(string s1, string s2)
         {
-            return s.Length >= 2 ? s.Substring(0, 2) : (s.Length == 1 ? s + '#' : "##");
+            string firstChar = s1.Length == 0 ? "#" : s1.Substring(0, 1);
+            string secondChar = s2.Length == 0 ? "#" : s2.Substring(s2.Length - 1);
+            return firstChar + secondChar;
         }
     }
 }
