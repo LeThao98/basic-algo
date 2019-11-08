1c31d39b1fcbc7e3b3b566e860d38c1e65f97c0d
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 11:16:50 +0700

    100

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,32 +5,27 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new array of integers and length 1 or more.
-       The length of the new array will be double length of an given array and
-       all the elements are 0 except the first element which is equal to first elements of the given array.
+       Write a C# Sharp program to check a given array of integers and return true if the array contains 10 or 20 twice.
+       The length of the array will be 0, 1, or 2.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 10, 20, -30, -40, 30 });
-            Console.Write("New array: ");
-            foreach (var i in item)
-            {
-                Console.Write(i.ToString() + " ");
-            }
+            Console.WriteLine(test(new[] { 12, 20 }));
+            Console.WriteLine(test(new[] { 20, 20 }));
+            Console.WriteLine(test(new[] { 10, 10 }));
+            Console.WriteLine(test(new[] { 10 }));
 
             Console.ReadKey();
         }
 
-        public static int[] test(int[] nums)
+        public static bool test(int[] nums)
         {
-            int elements = nums.Length * 2;
-            int[] doubleNums = new int[elements];
-
-            doubleNums[0] = nums[0];
-            return doubleNums;
+            return nums.Length == 2
+               && ((nums[0] == 10 && nums[1] == 10)
+                    || (nums[0] == 20 && nums[1] == 20));
         }
     }
 }
