3f285e1d1718ca5ae4a15614362e58d56cab2d1c
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 10:39:26 +0700

    083

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,31 +5,24 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to check whether the first two characters and last two characters of a given string are same.
+       Write a C# Sharp program to create a new string using 3 copies of the first 2 characters of a given string.
+       If the length of the given string is less than 2 use the whole string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test("abc", "abcd"));
-            Console.WriteLine(test("Python", "Python"));
-            Console.WriteLine(test("JS", "Python"));
+            Console.WriteLine(test("abc"));
+            Console.WriteLine(test("Python"));
+            Console.WriteLine(test("J"));
 
             Console.ReadKey();
         }
 
-        public static string test(string s1, string s2)
+        public static string test(string s)
         {
-            if (s1.Length > s2.Length)
-            {
-                return s1.Substring(0, s2.Length) + s2;
-            }
-            if (s2.Length > s1.Length)
-            {
-                return s1 + s2.Remove(0, s2.Length - s1.Length);
-            }
-            return s1 + s2;
+            return s.Length >= 2 ? s.Substring(0, 2) + s.Substring(0, 2) + s.Substring(0, 2) : s + s + s;
         }
     }
 }
