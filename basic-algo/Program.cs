7ff9fb94cc3d785b51cc732eba3415f770244581
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 22:22:53 +0700

    107

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,27 +5,43 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-      Write a C# Sharp program to create a new array of length 3 from a given array (length atleast 3)
-      using the elements from the middle of the array.
+        Write a C# Sharp program to find the largest value from first, last, and middle elements
+        of a given array of integers of odd length (atleast 1).
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 1, 5, 7, 9, 11, 13 });
-            Console.Write("New array: ");
-            foreach (var i in item)
-            {
-                Console.Write(i.ToString() + " ");
-            }
+            Console.WriteLine(test(new[] { 1 }));
+            Console.WriteLine(test(new[] { 1, 2, 9 }));
+            Console.WriteLine(test(new[] { 1, 2, 9, 3, 3 }));
+            Console.WriteLine(test(new[] { 1, 2, 3, 4, 5, 6, 7 }));
+            Console.WriteLine(test(new[] { 1, 2, 2, 3, 7, 8, 9, 10, 6, 5, 4 }));
 
             Console.ReadKey();
         }
 
-        public static int[] test(int[] numbers)
+        public static int test(int[] numbers)
         {
-            return new int[] { numbers[numbers.Length / 2 - 1], numbers[numbers.Length / 2], numbers[numbers.Length / 2 + 1] };
+            int first, middle_ele, last_ele, max_ele;
+
+            first = numbers[0];
+            middle_ele = numbers[numbers.Length / 2];
+            last_ele = numbers[numbers.Length - 1];
+            max_ele = first;
+
+            if (middle_ele > max_ele)
+            {
+                max_ele = middle_ele;
+            }
+
+            if (last_ele > max_ele)
+            {
+                max_ele = last_ele;
+            }
+
+            return max_ele;
         }
     }
 }
