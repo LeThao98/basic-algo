0ede544843800e38bc5d21a88bd33dbafcd4e1f8
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 22:33:15 +0700

    109

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,43 +5,28 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to find the largest value from first, last, and middle elements
-        of a given array of integers of odd length (atleast 1).
+        Write a C# Sharp program to create a new array taking the first two elements from a given array.
+        If the length of the given array is less than 2 then return the give array.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 1 }));
-            Console.WriteLine(test(new[] { 1, 2, 9 }));
-            Console.WriteLine(test(new[] { 1, 2, 9, 3, 3 }));
-            Console.WriteLine(test(new[] { 1, 2, 3, 4, 5, 6, 7 }));
-            Console.WriteLine(test(new[] { 1, 2, 2, 3, 7, 8, 9, 10, 6, 5, 4 }));
+            Console.WriteLine(test(new[] { 1, 5, 7, 9, 10, 12 }));
 
             Console.ReadKey();
         }
 
         public static int test(int[] numbers)
         {
-            int first, middle_ele, last_ele, max_ele;
+            int evens = 0;
 
-            first = numbers[0];
-            middle_ele = numbers[numbers.Length / 2];
-            last_ele = numbers[numbers.Length - 1];
-            max_ele = first;
-
-            if (middle_ele > max_ele)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                max_ele = middle_ele;
+                if (numbers[i] % 2 == 0) evens++;
             }
-
-            if (last_ele > max_ele)
-            {
-                max_ele = last_ele;
-            }
-
-            return max_ele;
+            return evens;
         }
     }
 }
