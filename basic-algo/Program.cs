bbb2769b922a5442cbcd89f1f0c65db337f58602
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 10:58:53 +0700

    091

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,24 +5,27 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to check two given arrays of integers of length 1 or more and return true if they have the same
-       first element or they have the same last element.
+       Write a C# Sharp program to compute the sum of the elements of an given array of integers.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 10, 20, 40, 50 }, new[] { 10, 20, 40, 50 }));
-            Console.WriteLine(test(new[] { 10, 20, 40, 50 }, new[] { 10, 20, 40, 5 }));
-            Console.WriteLine(test(new[] { 10, 20, 40, 50 }, new[] { 1, 20, 40, 5 }));
+            Console.WriteLine(test(new[] { 10, 20, 30, 40, 50 }));
+            Console.WriteLine(test(new[] { 10, 20, -30, -40, 50 }));
 
             Console.ReadKey();
         }
 
-        public static bool test(int[] a1, int[] a2)
+        public static int test(int[] a1)
         {
-            return a1[0] == a2[0] || a1[a1.Length - 1] == a2[a2.Length - 1];
+            int result = 0;
+            foreach (int item in a1)
+            {
+                result += item;
+            }
+            return result;
         }
     }
 }
