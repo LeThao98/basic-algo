e0a16c51086a978696768dc83100c1801a1309c4
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 10:52:06 +0700

    087

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,8 +5,8 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new string from a given string without the first and last character
-       if the first or last characters are 'a' otherwise return the original given string.
+       Write a C# Sharp program to create a new string from a given string.
+       If the first or first two characters is 'a', return the string without those 'a' characters otherwise return the original given string.
     */
 
     internal class Program
@@ -15,7 +15,7 @@ namespace basic_algo
         {
             Console.WriteLine(test("abcab"));
             Console.WriteLine(test("python"));
-            Console.WriteLine(test("abcda"));
+            Console.WriteLine(test("aacda"));
             Console.WriteLine(test("jython"));
 
             Console.ReadKey();
@@ -23,14 +23,22 @@ namespace basic_algo
 
         public static string test(string s)
         {
-            if (s.Length > 0 && s.Substring(s.Length - 1) == "a")
-            {
-                s = s.Remove(s.Length - 1);
-            }
-
-            if (s.Length > 0 && s.Substring(0, 1) == "a")
+            if (s.Length == 1 && s.Substring(0, 1) == "a")
             {
                 s = s.Remove(0, 1);
+            }
+
+            if (s.Length > 1)
+            {
+                if (s.Substring(1, 1) == "a")
+                {
+                    s = s.Remove(1, 1);
+                }
+
+                if (s.Substring(0, 1) == "a")
+                {
+                    s = s.Remove(0, 1);
+                }
             }
 
             return s;
