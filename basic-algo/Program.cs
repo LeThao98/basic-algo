982e105abd6b530401b37bb714517f36c9f77b64
LeThao98 <ledinhthao131098@gmail.com>
2019-11-08 22:04:47 +0700

    104

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,14 +5,14 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-      Write a C# Sharp program to create an array taking two middle elements from a given array of integers of length even.
+      Write a C# Sharp program to create a new array from two give array of integers, each length 3.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 1, 5, 7, 9, 11, 13 });
+            int[] item = test(new[] { 10, 20, 30 }, new[] { 40, 50, 60 });
             Console.Write("New array: ");
             foreach (var i in item)
             {
@@ -22,9 +22,9 @@ namespace basic_algo
             Console.ReadKey();
         }
 
-        public static int[] test(int[] numbers)
+        public static int[] test(int[] nums1, int[] nums2)
         {
-            return new int[] { numbers[numbers.Length / 2 - 1], numbers[numbers.Length / 2] };
+            return new int[] { nums1[0], nums1[1], nums1[2], nums2[0], nums2[1], nums2[2] };
         }
     }
 }
