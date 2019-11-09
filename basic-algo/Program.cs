63e7b7f38321c1effb521a3517755ee4018457ab
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 18:54:14 +0700

    132

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new array after replacing all the values 5 with 0 shifting all zeros to right direction.
+        Write a C# Sharp program to create new array from a given array of integers shifting all even numbers before all odd numbers.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 1, 2, 5, 3, 5, 7, 5, 9, 11 });
+            int[] item = test(new[] { 1, 2, 5, 3, 5, 4, 6, 9, 11 });
             Console.Write("New array: ");
             foreach (var i in item)
             {
@@ -21,16 +23,19 @@ namespace basic_algo
 
         public static int[] test(int[] numbers)
         {
-            int flag = 0;
-            int[] res = new int[numbers.Length];
-            for (int i = 0; i < res.Length; i++)
+            int index = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if (numbers[i] != 5)
+                if (numbers[i] % 2 == 0)
                 {
-                    res[flag++] = numbers[i];
+                    int temp = numbers[index];
+                    numbers[index] = numbers[i];
+                    numbers[i] = temp;
+
+                    index++;
                 }
             }
-            return res;
+            return numbers;
         }
     }
 }
