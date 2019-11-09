23611b580ac947ed611cf44f0ad6172cdd697f0a
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 17:07:48 +0700

    130

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
 
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new array taking the elements after the element value 5 from a given array of integers.
+        Write a C# Sharp program to create a new array from a given array of integers shifting all zeros to left direction.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 1, 2, 3, 5, 7, 9, 11 });
+            int[] item = test(new[] { 1, 2, 0, 3, 5, 7, 0, 9, 11 });
             Console.Write("New array: ");
             foreach (var i in item)
             {
@@ -23,21 +21,16 @@ namespace basic_algo
 
         public static int[] test(int[] numbers)
         {
-            int len = numbers.Length, size = 0, i = len - 1;
-            int[] post_ele_5;
-
-            while (i >= 0 && numbers[i] != 5) i--;
-            i++;
-
-            size = len - i;
-            post_ele_5 = new int[size];
-
-            for (int j = 0; j < size; j++)
+            int pos = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                post_ele_5[j] = numbers[i + j];
+                if (numbers[i] == 0)
+                {
+                    numbers[i] = numbers[pos];
+                    numbers[pos++] = 0;
+                }
             }
-
-            return post_ele_5;
+            return numbers;
         }
     }
 }
