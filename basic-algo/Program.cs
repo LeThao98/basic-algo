bd7b88e4f832389e8be354f3de22dcd2395bef54
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 21:27:31 +0700

    140

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,27 +5,37 @@ using System.Collections.Generic;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new array from a given array of strings using all the strings whose length are matched with given string length.
+        Write a C# Sharp program to create a new array of given length using the odd numbers from a given array of positive integers.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(123));
-            Console.WriteLine(test(13));
-            Console.WriteLine(test(222));
+            int[] item = test(new[] { 1, 2, 3, 5, 7, 9, 10 }, 3);
+            Console.Write("New array: ");
+            foreach (var i in item)
+            {
+                Console.Write(i.ToString() + " ");
+            }
             Console.ReadKey();
         }
 
-        public static bool test(int n)
+        public static int[] test(int[] nums, int count)
         {
-            while (n > 0)
+            int[] evens = new int[count];
+            int j = 0;
+
+            for (int i = 0; j < count; i++)
             {
-                if (n % 10 == 2) return true;
-                n /= 10;
+                if (nums[i] % 2 != 0)
+                {
+                    evens[j] = nums[i];
+                    j++;
+                }
             }
-            return false;
+
+            return evens;
         }
     }
 }
