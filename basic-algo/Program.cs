572f7c5739bfbba596786e18992f89d7a6acdf82
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 09:18:21 +0700

    120

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,14 +5,14 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check if an array of integers contains a 3 next to a 3 or a 5 next to a 5 or both.
+        Write a C# Sharp program to check a given array of integers and return true if the given array contains two 5's next to each other, or two 5 separated by one element.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 5, 5, 5, 5, 5 }));
+            Console.WriteLine(test(new[] { 5, 5, 1, 5, 5 }));
             Console.WriteLine(test(new[] { 1, 2, 3, 4 }));
             Console.WriteLine(test(new[] { 3, 3, 5, 5, 5, 5 }));
             Console.WriteLine(test(new[] { 1, 5, 5, 7, 8, 10 }));
@@ -21,7 +21,14 @@ namespace basic_algo
 
         public static bool test(int[] numbers)
         {
-            for (int i = 0; i < numbers.Length - 1; i++) if ((numbers[i] == 3 && numbers[i + 1] == 3) || (numbers[i] == 5 && numbers[i] == 5)) return true;
+            int len = numbers.Length;
+
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (numbers[i] == 5 && numbers[i + 1] == 5) return true;
+                if (i + 2 < len && numbers[i] == 5 && numbers[i + 2] == 5) return true;
+            }
+
             return false;
         }
     }
