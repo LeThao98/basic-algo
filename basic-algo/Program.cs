599e46c4af82b57e8c4c4ee4e92c671671d11ad3
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 10:11:40 +0700

    125

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,36 +5,33 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check a given array of integers and return true if every 5 that appears
-        in the given array is next to another 5.
+        Write a C# Sharp program to check a given array of integers and return true if the specified number of same elements
+        appears at the start and end of the given array.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 3, 5, 5, 3, 7 }));
-            Console.WriteLine(test(new[] { 3, 5, 5, 4, 1, 5, 7 }));
-            Console.WriteLine(test(new[] { 3, 5, 5, 5, 5, 5 }));
-            Console.WriteLine(test(new[] { 2, 4, 5, 5, 6, 7, 5 }));
+            Console.WriteLine(test(new[] { 3, 7, 5, 5, 3, 7 }, 2));
+            Console.WriteLine(test(new[] { 3, 7, 5, 5, 3, 7 }, 3));
+            Console.WriteLine(test(new[] { 3, 7, 5, 5, 3, 7, 5 }, 3));
             Console.ReadKey();
         }
 
-        public static bool test(int[] numbers)
+        public static bool test(int[] numbers, int len)
         {
-            int arr_len = numbers.Length;
-            bool flag = true;
+            int arra_size = numbers.Length;
 
-            for (int i = 0; i < arr_len; i++)
+            for (int i = 0; i < len; i++)
             {
-                if (numbers[i] == 5)
+                if (numbers[i] != numbers[arra_size - len + i])
                 {
-                    if ((i > 0 && numbers[i - 1] == 5) || (i < arr_len - 1 && numbers[i + 1] == 5)) flag = true;
-                    else if (i == arr_len - 1) flag = false;
-                    else return false;
+                    return false;
                 }
             }
-            return flag;
+
+            return true;
         }
     }
 }
