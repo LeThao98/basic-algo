686f82cbd37ff31492e06f997fa1dede5e1d70f8
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 10:04:07 +0700

    123

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,50 +5,40 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check a given array of integers and return true if the given array contains either 2 even or 2 odd
-        values all next to each other.
+        Write a C# Sharp program to check a given array of integers and return true if the value 5 appears 5 times
+        and there are no 5 next to each other.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 3, 5, 1, 3, 7 }));
-            Console.WriteLine(test(new[] { 1, 2, 3, 4 }));
-            Console.WriteLine(test(new[] { 3, 3, 5, 5, 5, 5 }));
-            Console.WriteLine(test(new[] { 2, 5, 5, 7, 8, 10 }));
+            Console.WriteLine(test(new[] { 3, 5, 1, 5, 3, 5, 7, 5, 1, 5 }));
+            Console.WriteLine(test(new[] { 3, 5, 5, 5, 5, 5, 5 }));
+            Console.WriteLine(test(new[] { 3, 5, 2, 5, 4, 5, 7, 5, 8, 5 }));
+            Console.WriteLine(test(new[] { 2, 4, 5, 5, 5, 5 }));
             Console.ReadKey();
         }
 
         public static bool test(int[] numbers)
         {
-            //int tot_odd = 0, tot_even = 0;
+            bool flag = false;
+            int five = 0;
 
-            //for (int i = 0; i < numbers.Length; i++)
-            //{
-            //    if (tot_odd < 2 && tot_even < 2)
-            //    {
-            //        if (numbers[i] % 2 == 0)
-            //        {
-            //            tot_even++;
-            //            tot_odd = 0;
-            //        }
-            //        else
-            //        {
-            //            tot_odd++;
-            //            tot_even = 0;
-            //        }
-            //    }
-            //}
-            //return tot_odd == 2 || tot_even == 2;
-            for (int i = 0; i < numbers.Length - 1; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if (numbers[i] % 2 == 0 && numbers[i + 1] % 2 == 0)
-                    return true;
-                if (numbers[i] % 2 != 0 && numbers[i + 1] % 2 != 0)
-                    return true;
+                if (numbers[i] == 5 && !flag)
+                {
+                    five++;
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                }
             }
-            return false;
+
+            return five == 5;
         }
     }
 }
