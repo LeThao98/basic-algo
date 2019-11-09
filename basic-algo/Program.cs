8040257811d40dbcb897039ef87a7552f79b4d7e
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 08:39:39 +0700

    115

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check whether a given array of integers contains 5's and 7's.
+        Write a C# Sharp program to check if the sum of all 5' in the array exactly 15 in a given array of integers.
     */
 
     internal class Program
@@ -13,19 +13,22 @@ namespace basic_algo
         private static void Main(string[] args)
         {
             Console.WriteLine(test(new[] { 1, 5, 6, 9, 10, 17 }));
-            Console.WriteLine(test(new[] { 1, 4, 7, 9, 10, 17 }));
-            Console.WriteLine(test(new[] { 1, 1, 2, 9, 10, 17 }));
+            Console.WriteLine(test(new[] { 1, 5, 5, 5, 10, 17 }));
+            Console.WriteLine(test(new[] { 1, 1, 5, 5, 5, 5 }));
             Console.ReadKey();
         }
 
         public static bool test(int[] nums)
         {
+            int sum = 0;
+
             for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] == 5 || nums[i] == 7) return true;
+                if (nums[i] == 5)
+                    sum += 5;
             }
 
-            return false;
+            return sum == 15;
         }
     }
 }
