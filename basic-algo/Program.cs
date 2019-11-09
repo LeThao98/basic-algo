5578c3ea0c62582001fa5c1a81ea04f6bce6ef97
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 20:20:07 +0700

    136

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,32 +5,27 @@ using System.Collections.Generic;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to find the larger average value between the first and the second half of a given array of integers and minimum length is atleast 2.
-        Assume that the second half begins at index (array length)/2.
+        Write a C# Sharp program to count the number of strings with given length in given array of strings.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 1, 2, 3, 4, 6, 8 }));
-            Console.WriteLine(test(new[] { 15, 2, 3, 4, 15, 11 }));
+            Console.WriteLine("Number of array: ");
+            Console.WriteLine(test(new[] { "a", "b", "bb", "c", "ccc" }, 1));
             Console.ReadKey();
         }
 
-        public static int test(int[] numbers)
+        public static int test(string[] arr_str, int len)
         {
-            var firstHalf = Average(numbers, 0, numbers.Length / 2);
-            var secondHalf = Average(numbers, numbers.Length / 2, numbers.Length);
-            return firstHalf > secondHalf ? firstHalf : secondHalf;
-        }
+            int ctr = 0;
 
-        private static int Average(int[] num, int start, int end)
-        {
-            var sum = 0;
-            for (var i = start; i < end; i++)
-                sum += num[i];
-            return sum / (num.Length / 2);
+            for (int i = 0; i < arr_str.Length; i++)
+            {
+                if (arr_str[i].Length == len) ctr++;
+            }
+            return ctr;
         }
     }
 }
