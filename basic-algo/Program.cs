a69bb7c889eb5a418f9a5b8c91f3e2696fc7ac1f
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 19:28:46 +0700

    133

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,37 +5,28 @@ using System.Collections.Generic;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create new array from a given array of integers shifting all even numbers before all odd numbers.
+        Write a C# Sharp program to check if the value of each element is equal or greater than the value of previous element of a given array of integers.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 1, 2, 5, 3, 5, 4, 6, 9, 11 });
-            Console.Write("New array: ");
-            foreach (var i in item)
-            {
-                Console.Write(i.ToString() + " ");
-            }
+            Console.WriteLine(test(new[] { 5, 5, 1, 5, 5 }));
+            Console.WriteLine(test(new[] { 1, 2, 3, 4 }));
+            Console.WriteLine(test(new[] { 3, 3, 5, 5, 5, 5 }));
+            Console.WriteLine(test(new[] { 1, 5, 5, 7, 8, 10 }));
             Console.ReadKey();
         }
 
-        public static int[] test(int[] numbers)
+        public static bool test(int[] numbers)
         {
-            int index = 0;
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
-                if (numbers[i] % 2 == 0)
-                {
-                    int temp = numbers[index];
-                    numbers[index] = numbers[i];
-                    numbers[i] = temp;
-
-                    index++;
-                }
+                if (numbers[i + 1] < numbers[i]) return false;
             }
-            return numbers;
+
+            return true;
         }
     }
 }
