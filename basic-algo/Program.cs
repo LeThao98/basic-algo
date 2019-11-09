bf3a92b32a8d7cd30a7770a57d56cca2955a27be
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 10:09:51 +0700

    124

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,40 +5,36 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check a given array of integers and return true if the value 5 appears 5 times
-        and there are no 5 next to each other.
+        Write a C# Sharp program to check a given array of integers and return true if every 5 that appears
+        in the given array is next to another 5.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 3, 5, 1, 5, 3, 5, 7, 5, 1, 5 }));
-            Console.WriteLine(test(new[] { 3, 5, 5, 5, 5, 5, 5 }));
-            Console.WriteLine(test(new[] { 3, 5, 2, 5, 4, 5, 7, 5, 8, 5 }));
-            Console.WriteLine(test(new[] { 2, 4, 5, 5, 5, 5 }));
+            Console.WriteLine(test(new[] { 3, 5, 5, 3, 7 }));
+            Console.WriteLine(test(new[] { 3, 5, 5, 4, 1, 5, 7 }));
+            Console.WriteLine(test(new[] { 3, 5, 5, 5, 5, 5 }));
+            Console.WriteLine(test(new[] { 2, 4, 5, 5, 6, 7, 5 }));
             Console.ReadKey();
         }
 
         public static bool test(int[] numbers)
         {
-            bool flag = false;
-            int five = 0;
+            int arr_len = numbers.Length;
+            bool flag = true;
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < arr_len; i++)
             {
-                if (numbers[i] == 5 && !flag)
+                if (numbers[i] == 5)
                 {
-                    five++;
-                    flag = true;
-                }
-                else
-                {
-                    flag = false;
+                    if ((i > 0 && numbers[i - 1] == 5) || (i < arr_len - 1 && numbers[i + 1] == 5)) flag = true;
+                    else if (i == arr_len - 1) flag = false;
+                    else return false;
                 }
             }
-
-            return five == 5;
+            return flag;
         }
     }
 }
