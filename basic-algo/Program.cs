274930d01ef6a0bd04c4ac53fd4512f2849fcc9d
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 17:41:42 +0700

    131

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -3,14 +3,14 @@
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new array from a given array of integers shifting all zeros to left direction.
+        Write a C# Sharp program to create a new array after replacing all the values 5 with 0 shifting all zeros to right direction.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            int[] item = test(new[] { 1, 2, 0, 3, 5, 7, 0, 9, 11 });
+            int[] item = test(new[] { 1, 2, 5, 3, 5, 7, 5, 9, 11 });
             Console.Write("New array: ");
             foreach (var i in item)
             {
@@ -21,16 +21,16 @@ namespace basic_algo
 
         public static int[] test(int[] numbers)
         {
-            int pos = 0;
-            for (int i = 0; i < numbers.Length; i++)
+            int flag = 0;
+            int[] res = new int[numbers.Length];
+            for (int i = 0; i < res.Length; i++)
             {
-                if (numbers[i] == 0)
+                if (numbers[i] != 5)
                 {
-                    numbers[i] = numbers[pos];
-                    numbers[pos++] = 0;
+                    res[flag++] = numbers[i];
                 }
             }
-            return numbers;
+            return res;
         }
     }
 }
