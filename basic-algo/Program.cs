ca6b19c473434c44eed207df63c6d8ef10a19bb3
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 08:07:07 +0700

    111

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,33 +5,26 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to compute the difference between the largest and
-        smallest values in a gvien array of integers and length one or more.
+        Write a C# Sharp program to compute the sum of values in a given array of integers except the number 17.
+        Return 0 if the given array has no integer.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Difference between the largest and smallest values: ");
-            Console.WriteLine(test(new[] { 1, 5, 7, 9, 10, 12 }));
-
             Console.ReadKey();
         }
 
         public static int test(int[] nums)
         {
-            int small_num = 0, biggest_num = 0;
+            int sum = 0;
 
-            if (nums.Length > 0) small_num = biggest_num = nums[0];
-
-            for (int i = 1; i < nums.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                small_num = Math.Min(small_num, nums[i]);
-                biggest_num = Math.Max(biggest_num, nums[i]);
+                if (nums[i] != 17) sum += nums[i];
             }
-
-            return biggest_num - small_num;
+            return sum;
         }
     }
 }
