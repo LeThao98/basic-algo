6996a97e257a8c320a1a427d3d1332cec0a099c6
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 09:04:21 +0700

    118

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check if the sum of all 5' in the array exactly 15 in a given array of integers.
+        Write a C# Sharp program to check if a given array of integers contains no 3 or a 5.
     */
 
     internal class Program
@@ -21,9 +21,14 @@ namespace basic_algo
 
         public static bool test(int[] nums)
         {
+            var three = false;
+            var five = false;
+
             for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] != 3 && nums[i] != 5) return false;
+                if (nums[i] == 3) { three = true; }
+                if (nums[i] == 5) { five = true; }
+                if (three && five) return false;
             }
             return true;
         }
