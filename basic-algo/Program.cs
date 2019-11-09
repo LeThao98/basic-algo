04be3b6f216d304bcd317bef9c5636a9f0bf60eb
LeThao98 <ledinhthao131098@gmail.com>
2019-11-09 19:29:55 +0700

    134

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -5,17 +5,18 @@ using System.Collections.Generic;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to check if the value of each element is equal or greater than the value of previous element of a given array of integers.
+        Write a C# Sharp program to check a given array (length will be atleast 2) of integers and return true
+        if there are two values 15, 15 next to each other.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(test(new[] { 5, 5, 1, 5, 5 }));
-            Console.WriteLine(test(new[] { 1, 2, 3, 4 }));
-            Console.WriteLine(test(new[] { 3, 3, 5, 5, 5, 5 }));
-            Console.WriteLine(test(new[] { 1, 5, 5, 7, 8, 10 }));
+            Console.WriteLine(test(new[] { 5, 5, 1, 15, 15 }));
+            Console.WriteLine(test(new[] { 15, 2, 3, 4, 15 }));
+            Console.WriteLine(test(new[] { 3, 3, 15, 15, 5, 5 }));
+            Console.WriteLine(test(new[] { 1, 5, 15, 7, 8, 15 }));
             Console.ReadKey();
         }
 
@@ -23,10 +24,9 @@ namespace basic_algo
         {
             for (int i = 0; i < numbers.Length - 1; i++)
             {
-                if (numbers[i + 1] < numbers[i]) return false;
+                if (numbers[i + 1] == numbers[i] && numbers[i] == 15) return true;
             }
-
-            return true;
+            return false;
         }
     }
 }
