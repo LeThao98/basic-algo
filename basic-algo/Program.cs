b984fe0e7555dcf19171079ae7e04e21e8f771ed
LeThao98 <ledinhthao131098@gmail.com>
2019-11-10 08:39:53 +0700

    144

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -6,8 +6,8 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new list from a given list of strings where each element
-        has "#" added at the beginning and end position.
+        Write a C# Sharp program to create a new list from a given list of strings where each element is replaced
+        by 4 copies of the string concatenated together.
     */
 
     internal class Program
@@ -24,7 +24,7 @@ namespace basic_algo
 
         public static List<string> test(List<string> nums_str)
         {
-            IEnumerable<string> s = nums_str.Select(x => "#" + x + "#");
+            IEnumerable<string> s = nums_str.Select(x => x = x + x + x + x);
             return s.ToList();
         }
     }
