20c0e510ea1312dde9b1845458ecfb73af142c54
LeThao98 <ledinhthao131098@gmail.com>
2019-11-10 08:44:32 +0700

    147

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -6,14 +6,14 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new list of the rightmost digits from a given list of positive integers.
+       Write a C# Sharp program to create a new list from a given list of strings where strings will be in upper case in new string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            List<int> mylist = test(new List<int>(new int[] { 10, 22, 35, 41 }));
+            List<string> mylist = test(new List<string>(new string[] { "Abc", "cdef", "js", "php" }));
             foreach (var i in mylist)
             {
                 Console.Write(i.ToString() + " ");
@@ -21,10 +21,10 @@ namespace basic_algo
             Console.ReadKey();
         }
 
-        public static List<int> test(List<int> nums)
+        public static List<string> test(List<string> str)
         {
-            IEnumerable<int> e = nums.Select(x => x % 10);
-            return e.ToList();
+            IEnumerable<string> s = str.Select(x => x.ToUpper());
+            return s.ToList();
         }
     }
 }
