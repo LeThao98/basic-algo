bad2500205a61a47db92428faaae36e21b24b284
LeThao98 <ledinhthao131098@gmail.com>
2019-11-10 08:45:45 +0700

    148

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -6,14 +6,14 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-       Write a C# Sharp program to create a new list from a given list of strings where strings will be in upper case in new string.
+       Write a C# Sharp program to remove all "a" in each string in given list of strings and return the new string.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            List<string> mylist = test(new List<string>(new string[] { "Abc", "cdef", "js", "php" }));
+            List<string> mylist = test(new List<string>(new string[] { "abc", "cdaef", "js", "php" }));
             foreach (var i in mylist)
             {
                 Console.Write(i.ToString() + " ");
@@ -23,8 +23,7 @@ namespace basic_algo
 
         public static List<string> test(List<string> str)
         {
-            IEnumerable<string> s = str.Select(x => x.ToUpper());
-            return s.ToList();
+            return str.Select(x => x.Replace("a", "")).ToList();
         }
     }
 }
