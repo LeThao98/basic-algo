ec92d08f6d0c2ed2a66648e5820ea4bddcd6f7ab
LeThao98 <ledinhthao131098@gmail.com>
2019-11-10 09:53:30 +0700

    data_types 01

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,13 +7,26 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-
+     * data-types
+        Write a C# Sharp program that takes three letters as input and display them in reverse order.
     */
 
-    internal class Program
+    public class Program
     {
         private static void Main(string[] args)
         {
+            char letter,letter1,letter2;
+  
+            Console.Write("Input letter: ");
+            letter = Convert.ToChar(Console.ReadLine());
+ 
+            Console.Write("Input letter: ");
+            letter1 = Convert.ToChar(Console.ReadLine());       
+ 
+            Console.Write("Input letter: ");
+            letter2 = Convert.ToChar(Console.ReadLine());
+             
+            Console.WriteLine("{0} {1} {2}",letter2,letter1,letter);
         }
     }
 }
