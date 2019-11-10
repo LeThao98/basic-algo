ccb8df5b0447349a701bec4d66930d59aec6f5bc
LeThao98 <ledinhthao131098@gmail.com>
2019-11-10 08:37:00 +0700

    142

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -6,14 +6,14 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new list from a given list of integers where each element is multiplied by 3.
+        Write a C# Sharp program to create a new list from a given list of integers where each integer multiplied by itself three times.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            List<int> mylist = test(new List<int>(new int[] { 1, 2, 3, 4 }));
+            List<int> mylist = test(new List<int>(new[] { 1, 2, 3, 4 }));
             foreach (var i in mylist)
             {
                 Console.Write(i.ToString() + " ");
@@ -23,8 +23,8 @@ namespace basic_algo
 
         public static List<int> test(List<int> nums)
         {
-            IEnumerable<int> doubled = nums.Select(x => x *= 3);
-            return doubled.ToList<int>();
+            IEnumerable<int> squared = nums.Select(x => x * x * x);
+            return squared.ToList<int>();
         }
     }
 }
