6956bd226c60a812ae1b3efd9209c746dc25c91a
LeThao98 <ledinhthao131098@gmail.com>
2019-11-10 08:38:24 +0700

    143

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -6,14 +6,15 @@ using System.Linq;
 namespace basic_algo
 {
     /*
-        Write a C# Sharp program to create a new list from a given list of integers where each integer multiplied by itself three times.
+        Write a C# Sharp program to create a new list from a given list of strings where each element
+        has "#" added at the beginning and end position.
     */
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            List<int> mylist = test(new List<int>(new[] { 1, 2, 3, 4 }));
+            List<string> mylist = test(new List<string>(new string[] { "1", "2", "3", "4" }));
             foreach (var i in mylist)
             {
                 Console.Write(i.ToString() + " ");
@@ -21,10 +22,10 @@ namespace basic_algo
             Console.ReadKey();
         }
 
-        public static List<int> test(List<int> nums)
+        public static List<string> test(List<string> nums_str)
         {
-            IEnumerable<int> squared = nums.Select(x => x * x * x);
-            return squared.ToList<int>();
+            IEnumerable<string> s = nums_str.Select(x => "#" + x + "#");
+            return s.ToList();
         }
     }
 }
