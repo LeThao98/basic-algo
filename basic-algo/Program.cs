30889d2b63ddb53d62f682ebb716ffccb4b07f58
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 07:55:38 +0700

    conditional-statement 02

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,28 +8,25 @@ namespace basic_algo
 {
     /*
         conditional-statement
-        Write a C# Sharp program to accept two integers and check whether they are equal or not.
+        Write a C program to check whether a given number is even or odd.
     */
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            int int1, int2;
+            int int1;
             Console.Write("\n\n");
-            Console.Write("Check whether two integers are equal or not:\n");
+            Console.Write("Check whether a number is even or odd :\n");
             Console.Write("-------------------------------------------");
             Console.Write("\n\n");
-            Console.Write("Input 1st number: ");
+            Console.Write("Input an number: ");
             int1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Input 2nd number: ");
-            int2 = Convert.ToInt32(Console.ReadLine());
-
-            if (int1 == int2)
-                Console.WriteLine("{0} and {1} are equal.\n", int1, int2);
+            if (int1 % 2 == 0)
+                Console.WriteLine("{0} is an even integer.\n", int1);
             else
-                Console.WriteLine("{0} and {1} are not equal.\n", int1, int2);
+                Console.WriteLine("{0} is an odd integer.\n", int1);
             Console.ReadKey();
         }
     }
