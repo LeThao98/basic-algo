0f6aaae1c37b8f1fca9e7b48a11aa3d305d1b03e
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 08:16:57 +0700

    conditional-statement 13

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,51 +8,40 @@ namespace basic_algo
 {
     /*
         conditional-statement
-        Write a C# Sharp program to read roll no, name and marks of three subjects and calculate the total, percentage and division.
+        Write a C# Sharp program to read temperature in centigrade and display suitable message according to temperature.
+
+          Temp < 0 then Freezing weather
+          Temp 0-10 then Very Cold weather
+          Temp 10-20 then Cold weather
+          Temp 20-30 then Normal in Temp
+          Temp 30-40 then Its Hot
+          Temp >=40 then Its Very Hot
     */
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            double rl, phy, che, ca, total;
-            double per;
-            string nm, div;
-
+            int tmp;
             Console.Write("\n\n");
-            Console.Write("Calculate the total, percentage and division to take marks of three subjects:\n");
-            Console.Write("-------------------------------------------------------------------------------");
+            Console.Write("Accept a temperature in centigrade and display a suitable message:\n");
+            Console.Write("--------------------------------------------------------------------");
             Console.Write("\n\n");
 
-            Console.Write("Input the Roll Number of the student :");
-            rl = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Input the Name of the Student :");
-            nm = Console.ReadLine();
-
-            Console.Write("Input  the marks of Physics : ");
-            phy = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input  the marks of  Chemistry : ");
-            che = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input  the marks of Computer Application : ");
-            ca = Convert.ToInt32(Console.ReadLine());
-
-            total = phy + che + ca;
-            per = total / 3.0;
-            if (per >= 60)
-                div = "First";
+            Console.Write("Input days temperature : ");
+            tmp = Convert.ToInt32(Console.ReadLine());
+            if (tmp < 0)
+                Console.Write("Freezing weather.\n");
+            else if (tmp < 10)
+                Console.Write("Very cold weather.\n");
+            else if (tmp < 20)
+                Console.Write("Cold weather.\n");
+            else if (tmp < 30)
+                Console.Write("Normal in temp.\n");
+            else if (tmp < 40)
+                Console.Write("Its Hot.\n");
             else
-            if (per < 60 && per >= 48)
-                div = "Second";
-            else
-                if (per < 48 && per >= 36)
-                div = "Pass";
-            else
-                div = "Fail";
-
-            Console.Write("\nRoll No : {0}\nName of Student : {1}\n", rl, nm);
-            Console.Write("Marks in Physics : {0}\nMarks in Chemistry : {1}\nMarks in Computer Application : {2}\n", phy, che, ca);
-            Console.Write("Total Marks = {0}\nPercentage = {1}\nDivision = {2}\n", total, per, div);
+                Console.Write("Its very hot.\n");
             Console.ReadKey();
         }
     }
