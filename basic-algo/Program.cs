5f36e744c27dcb6d576d49bec15de3aa472f5206
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 08:56:02 +0700

    conditional-statement 21

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -15,48 +15,60 @@ namespace basic_algo
     {
         private static void Main(string[] args)
         {
-            int dayno;
+            int cdigit;
 
             Console.Write("\n\n");
-            Console.Write("Accept day number and display its equivalent day name in word:\n");
-            Console.Write("----------------------------------------------------------------");
+            Console.Write("Accept digit and display in word:\n");
+            Console.Write("-----------------------------------");
             Console.Write("\n\n");
 
-            Console.Write("Input Day No : ");
-            dayno = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input Digit(0-9) : ");
+            cdigit = Convert.ToInt32(Console.ReadLine());
 
-            switch (dayno)
+            switch (cdigit)
             {
+                case 0:
+                    Console.Write("Zero\n");
+                    break;
+
                 case 1:
-                    Console.Write("Monday \n");
+                    Console.Write("one\n");
                     break;
 
                 case 2:
-                    Console.Write("Tuesday \n");
+                    Console.Write("Two\n");
                     break;
 
                 case 3:
-                    Console.Write("Wednesday \n");
+                    Console.Write("Three\n");
                     break;
 
                 case 4:
-                    Console.Write("Thursday \n");
+                    Console.Write("Four\n");
                     break;
 
                 case 5:
-                    Console.Write("Friday \n");
+                    Console.Write("Five\n");
                     break;
 
                 case 6:
-                    Console.Write("Saturday \n");
+                    Console.Write("Six\n");
                     break;
 
                 case 7:
-                    Console.Write("Sunday  \n");
+                    Console.Write("Seven\n");
+                    break;
+
+                case 8:
+                    Console.Write("Eight\n");
+                    break;
+
+                case 9:
+                    Console.Write("Nine\n");
                     break;
 
                 default:
-                    Console.Write("Invalid day number. \nPlease try again ....\n");
+                    Console.Write("invalid digit. \nPlease try again ....\n");
                     break;
             }
 
