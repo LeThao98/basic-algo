02236e33af43e2d9655c6eac14305e68a22bfdc2
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 08:31:08 +0700

    conditional-statement 15

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,39 +8,39 @@ namespace basic_algo
 {
     /*
         conditional-statement
-        Write a C# Sharp program to check whether a triangle is Equilateral, Isosceles or Scalene.
+        Write a C# Sharp program to check whether a triangle can be formed by the given value for the angles.
     */
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            int sidea, sideb, sidec;
+            int anga, angb, angc, sum; //are three angles of a triangle
             Console.Write("\n\n");
-            Console.Write("Check whether a triangle is Equilateral, Isosceles or Scalene:\n");
-            Console.Write("----------------------------------------------------------------");
+            Console.Write("Check whether a triangle can be formed by given value:\n");
+            Console.Write("--------------------------------------------------------");
             Console.Write("\n\n");
 
-            Console.Write("Input side 1 of triangle: ");
-            sidea = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input angle1 of triangle: ");
+            anga = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Input side 2 of triangle: ");
-            sideb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input angle 2 of triangle: ");
+            angb = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Input side 3 of triangle: ");
-            sidec = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input angle 3 of triangle: ");
+            angc = Convert.ToInt32(Console.ReadLine());
 
-            if (sidea == sideb && sideb == sidec)
+            /* Calculate the sum of all angles of triangle */
+            sum = anga + angb + angc;
+
+            /* Check whether sum=180 then its a valid triangle otherwise not */
+            if (sum == 180)
             {
-                Console.Write("This is an equilateral triangle.\n");
-            }
-            else if (sidea == sideb || sidea == sidec || sideb == sidec)
-            {
-                Console.Write("This is an isosceles triangle.\n");
+                Console.Write("The triangle is valid.\n");
             }
             else
             {
-                Console.Write("This is a scalene triangle.\n");
+                Console.Write("The triangle is not valid.\n");
             }
             Console.ReadKey();
         }
