00609f8360f6f3fdcd9363521852b9d46ea0b43a
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 08:07:43 +0700

    conditional-statement 09

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,41 +8,35 @@ namespace basic_algo
 {
     /*
         conditional-statement
-        Write a C program to find the largest of three numbers.
+        Write a C program to accept a coordinate point in an XY coordinate system and determine in which quadrant the coordinate point lies.
     */
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            int num1, num2, num3;
+            int co1, co2;
+
             Console.Write("\n\n");
-            Console.Write("Find the largest of three numbers:\n");
-            Console.Write("------------------------------------");
+            Console.Write("Find the quadrant in which the coordinate point lies:\n");
+            Console.Write("------------------------------------------------------");
             Console.Write("\n\n");
 
-            Console.Write("Input the 1st number :");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the  2nd number :");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the 3rd  number :");
-            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value for X coordinate :");
+            co1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value for Y coordinate :");
+            co2 = Convert.ToInt32(Console.ReadLine());
 
-            if (num1 > num2)
-            {
-                if (num1 > num3)
-                {
-                    Console.Write("The 1st Number is the greatest among three. \n\n");
-                }
-                else
-                {
-                    Console.Write("The 3rd Number is the greatest among three. \n\n");
-                }
-            }
-            else if (num2 > num3)
-                Console.Write("The 2nd Number is the greatest among three \n\n");
-            else
-                Console.Write("The 3rd Number is the greatest among three \n\n");
+            if (co1 > 0 && co2 > 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the First quandrant.\n\n", co1, co2);
+            else if (co1 < 0 && co2 > 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Second quandrant.\n\n", co1, co2);
+            else if (co1 < 0 && co2 < 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Third quandrant.\n\n", co1, co2);
+            else if (co1 > 0 && co2 < 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Fourth quandrant.\n\n", co1, co2);
+            else if (co1 == 0 && co2 == 0)
+                Console.Write("The coordinate point ({0} {1}) lies at the origin.\n\n", co1, co2);
             Console.ReadKey();
         }
     }
