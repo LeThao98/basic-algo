31af6134c38eb0034bfd23181d16cee1e4fcbe98
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 08:57:48 +0700

    conditional-statement 23

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,7 +8,7 @@ namespace basic_algo
 {
     /*
         conditional-statement
-        Write a program in C# Sharp to read any Month Number in integer and display Month name in the word.
+        Write a program in C# Sharp to read any Month Number in integer and display the number of days for this month.
     */
 
     public class Program
@@ -18,65 +18,38 @@ namespace basic_algo
             int monno;
 
             Console.Write("\n\n");
-            Console.Write("Read month number and display month name:\n");
-            Console.Write("-------------------------------------------");
+            Console.Write("Read month number and display number of days for that month:\n");
+            Console.Write("--------------------------------------------------------------");
             Console.Write("\n\n");
 
             Console.Write("Input Month No : ");
             monno = Convert.ToInt32(Console.ReadLine());
-
             switch (monno)
             {
                 case 1:
-                    Console.Write("January\n");
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.Write("Month  have 31 days. \n");
                     break;
 
                 case 2:
-                    Console.Write("February\n");
-                    break;
-
-                case 3:
-                    Console.Write("March\n");
+                    Console.Write("The 2nd month is a February and have 28 days. \n");
+                    Console.Write("in leap year The February month  Have 29 days.\n");
                     break;
 
                 case 4:
-                    Console.Write("April\n");
-                    break;
-
-                case 5:
-                    Console.Write("May\n");
-                    break;
-
                 case 6:
-                    Console.Write("June\n");
-                    break;
-
-                case 7:
-                    Console.Write("July\n");
-                    break;
-
-                case 8:
-                    Console.Write("August\n");
-                    break;
-
                 case 9:
-                    Console.Write("September\n");
-                    break;
-
-                case 10:
-                    Console.Write("October\n");
-                    break;
-
                 case 11:
-                    Console.Write("November\n");
-                    break;
-
-                case 12:
-                    Console.Write("December\n");
+                    Console.Write("Month have 30 days. \n");
                     break;
 
                 default:
-                    Console.Write("invalid Month number. \nPlease try again ....\n");
+                    Console.Write("invalid Month number.\nPlease try again ....\n");
                     break;
             }
 
