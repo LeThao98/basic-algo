7ae2e754128594e9ea804f53147373621cfc93c8
LeThao98 <ledinhthao131098@gmail.com>
2019-11-11 08:00:22 +0700

    conditional-statement 05

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,29 +8,28 @@ namespace basic_algo
 {
     /*
         conditional-statement
-        Write a C# Sharp program to check whether a given number is positive or negative.
+        Write a C# Sharp program to read age of a candidate and determine whether it is eligible for casting his/her own vote.
     */
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            int chk_year;
+            int vote_age;
             Console.Write("\n\n");
-            Console.Write("Check whether a given year is leap year or not:\n");
-            Console.Write("----------------------------------------------");
+            Console.Write("Detrermine a specific age is eligible for casting the vote:\n");
+            Console.Write("----------------------------------------------------------");
             Console.Write("\n\n");
-            Console.Write("Input an year : ");
-            chk_year = Convert.ToInt32(Console.ReadLine());
 
-            if ((chk_year % 400) == 0)
-                Console.WriteLine("{0} is a leap year.\n", chk_year);
-            else if ((chk_year % 100) == 0)
-                Console.WriteLine("{0} is not a leap year.\n", chk_year);
-            else if ((chk_year % 4) == 0)
-                Console.WriteLine("{0} is a leap year.\n", chk_year);
+            Console.Write("Input the age of the candidate : ");
+            vote_age = Convert.ToInt32(Console.ReadLine());
+            if (vote_age < 18)
+            {
+                Console.Write("Sorry, You are not eligible to caste your vote.\n");
+                Console.Write("You would be able to caste your vote after {0} year.\n\n", 18 - vote_age);
+            }
             else
-                Console.WriteLine("{0} is not a leap year.\n", chk_year);
+                Console.Write("Congratulation! You are eligible for casting your vote.\n\n");
             Console.ReadKey();
         }
     }
