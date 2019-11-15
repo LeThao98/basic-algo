c7246df5206860e89f4f3571b64c1c3c8e553e1a
LeThao98 <ledinhthao131098@gmail.com>
2019-11-15 09:44:09 +0700

    searching-and-sorting-algorithm: Counting Sort

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,36 +7,44 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-        searching-and-sorting-algorithm: Bubble  Sort
-        Write a C# Sharp program to sort a list of elements using Bubble sort.
+        searching-and-sorting-algorithm: Counting Sort
+        Write a C# Sharp program to sort a list of elements using Counting sort.
     */
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            int[] a = { 3, 0, 2, 5, -1, 4, 1 };
-            int t;
-            Console.WriteLine("Original array :");
-            foreach (int aa in a)
+            int[] array = new int[10] { 2, 5, -4, 11, 0, 8, 22, 67, 51, 6 };
+            int[] sortedArray = new int[array.Length];
+            Console.WriteLine("\n" + "Original array :");
+            foreach (int aa in array)
                 Console.Write(aa + " ");
-            for (int p = 0; p <= a.Length - 2; p++)
+            int minVal = array[0], maxVal = array[0];
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int i = 0; i <= a.Length - 2; i++)
-                {
-                    if (a[i] > a[i + 1])
-                    {
-                        t = a[i + 1];
-                        a[i + 1] = a[i];
-                        a[i] = t;
-                    }
-                }
+                if (array[i] > maxVal) maxVal = array[i];
+                if (array[i] < minVal) minVal = array[i];
             }
+            int[] counts = new int[maxVal - minVal + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                counts[array[i] - minVal]++;
+            }
+            counts[0]--;
+            for (int i = 1; i < counts.Length; i++)
+            {
+                counts[i] = counts[i] + counts[i - 1];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                sortedArray[counts[array[i] - minVal]--] = array[i];
+            }
+            Console.Write(counts);
             Console.WriteLine("\n" + "Sorted array :");
-            foreach (int aa in a)
+            foreach (int aa in sortedArray)
                 Console.Write(aa + " ");
             Console.Write("\n");
-
             Console.ReadKey();
         }
     }
