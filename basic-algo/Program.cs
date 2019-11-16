28281a04152593cd4ed3822ac98f410efe1b50b6
LeThao98 <ledinhthao131098@gmail.com>
2019-11-16 15:09:51 +0700

    structure 03

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,26 +8,50 @@ namespace basic_algo
 {
     /*
         structure
-        Write a program in C# Sharp to declare a simple structure and use of static fields inside a struct.
-    */
+        Write a program in C# Sharp to create a nested struct to store two data for an employee in an array.
+     */
 
-    internal struct w3rStruct
+    internal struct employee
     {
-        public static int x = 15;
-        public static int y = 25;
-        public int z;
+        public string eName;
+        public dtObirth Date;
+    }
+
+    internal struct dtObirth
+    {
+        public int Day;
+        public int Month;
+        public int Year;
     }
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.Write("\n\nStructure with the use of static fields inside a struct :\n");
-            Console.Write("--------------------------------\n");
-            int sum = w3rStruct.x + w3rStruct.y;
-            w3rStruct w3R = new w3rStruct();
-            w3R.z = sum;
-            Console.WriteLine("The sum of x and y is {0}\n", w3R.z);
+            int dd = 0, mm = 0, yy = 0;
+            int total = 2;
+            Console.Write("\n\nCreate a nested struct and store data in an array :\n");
+            Console.Write("-------------------------------------------------------\n");
+            employee[] emp = new employee[total];
+            for (int i = 0; i < total; i++)
+            {
+                Console.Write("Name of the employee : ");
+                string nm = Console.ReadLine();
+                emp[i].eName = nm;
+
+                Console.Write("Input day of the birth : ");
+                dd = Convert.ToInt32(Console.ReadLine());
+                emp[i].Date.Day = dd;
+
+                Console.Write("Input month of the birth : ");
+                mm = Convert.ToInt32(Console.ReadLine());
+                emp[i].Date.Month = mm;
+
+                Console.Write("Input year for the birth : ");
+                yy = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                emp[i].Date.Year = yy;
+            }
             Console.ReadKey();
         }
     }
