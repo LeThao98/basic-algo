b936a756961adc939abca871d4dd60b97065e6b1
LeThao98 <ledinhthao131098@gmail.com>
2019-11-16 14:57:13 +0700

    structure 01

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,63 +7,29 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-        searching-and-sorting-algorithm: Selection Sort
-        Write a C# Sharp program to sort a list of elements using Selection sort.
+        structure
+        Write a program in C# Sharp to declare a simple structure.
     */
+
+    internal struct w3rStruct
+    {
+        public int x;
+        public int y;
+        private int z;
+    }
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            SelectionSort selectionSort = new SelectionSort(10);
-            selectionSort.Sort();
+            Console.Write("\n\nDeclare a simple structure :\n");
+            Console.Write("--------------------------------\n");
+            w3rStruct w3st = new w3rStruct();
+            w3st.x = 15;
+            w3st.y = 25;
+            int sum = w3st.x + w3st.y;
+            Console.WriteLine("The sum of x and y is {0}\n", sum);
             Console.ReadKey();
-        }
-    }
-
-    public class SelectionSort
-    {
-        private List<int> list = new List<int>();
-
-        public SelectionSort(int Length)
-        {
-            Random random = new Random();
-            Console.Write("Original Array: ");
-            for (int i = 0; i < Length; i++)
-            {
-                list.Add(random.Next(0, 100));
-                Console.Write("{0} ", list[i]);
-            }
-        }
-
-        private void Swap(int a, int b)
-        {
-            int temp = list[a];
-            list[a] = list[b];
-            list[b] = temp;
-        }
-
-        public void Sort()
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                int minIndex = i;
-                for (int j = i; j < list.Count; j++)
-                {
-                    if (list[j] < list[minIndex]) minIndex = j;
-                }
-                Swap(i, minIndex);
-                Print();
-            }
-        }
-
-        private void Print()
-        {
-            Console.WriteLine();
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.Write("{0} ", list[i]);
-            }
         }
     }
 }
