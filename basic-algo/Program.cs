c72a09075a43a0e4a0b501638cc47293ec715121
LeThao98 <ledinhthao131098@gmail.com>
2019-11-16 11:26:12 +0700

    searching-and-sorting-algorithm: Radix Sort

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-        searching-and-sorting-algorithm: Quick Sort
-        Write a C# Sharp program to sort a list of elements using Quick sort.
+        searching-and-sorting-algorithm: Radix Sort
+        Write a C# Sharp program to sort a list of elements using Radix sort.
     */
 
     public class Program
@@ -16,55 +16,40 @@ namespace basic_algo
         private static void Main(string[] args)
         {
             int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
-
-            Console.WriteLine("Original array : ");
+            Console.WriteLine("\nOriginal array : ");
             foreach (var item in arr)
             {
                 Console.Write(" " + item);
             }
-            Console.WriteLine();
 
-            QuickSort(arr, arr.Length - 1);
-
-            Console.WriteLine();
-            Console.WriteLine("Sorted array : ");
-
+            RadixSort(arr);
+            Console.WriteLine("\nSorted array : ");
             foreach (var item in arr)
             {
                 Console.Write(" " + item);
             }
-            Console.WriteLine();
+            Console.WriteLine("\n");
 
             Console.ReadKey();
         }
 
-        private static void swap(ref int a, ref int b)
+        private static void RadixSort(int[] arr)
         {
-            var temp = a;
-            a = b;
-            b = temp;
-        }
-
-        private static void QuickSort(int[] a, int maxIndex, int minIndex = 0)
-        {
-            Random random = new Random();
-            int privotIndex = random.Next(maxIndex - minIndex);
-            int privot = a[minIndex + privotIndex];
-            int i = minIndex;
-            int j = maxIndex;
-            while (i <= j)
+            int i, j;
+            int[] tmp = new int[arr.Length];
+            for (int shift = 31; shift > -1; --shift)
             {
-                while (a[i] < privot) i++;
-                while (a[j] > privot) j--;
-                if (i <= j)
+                j = 0;
+                for (i = 0; i < arr.Length; ++i)
                 {
-                    swap(ref a[i], ref a[j]);
-                    i++;
-                    j--;
+                    bool move = (arr[i] << shift) >= 0;
+                    if (shift == 0 ? !move : move)
+                        arr[i - j] = arr[i];
+                    else
+                        tmp[j++] = arr[i];
                 }
+                Array.Copy(tmp, 0, arr, arr.Length - j, j);
             }
-            if (j > minIndex) QuickSort(a, j);
-            if (i < maxIndex) QuickSort(a, maxIndex, i);
         }
     }
 }
