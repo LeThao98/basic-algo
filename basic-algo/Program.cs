bb751850f2389a503fe0c45a5a7ad1a987273666
LeThao98 <ledinhthao131098@gmail.com>
2019-11-16 15:20:31 +0700

    structure 05

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,45 +8,47 @@ namespace basic_algo
 {
     /*
         structure
-        Write a program in C# Sharp to create a structure and assign the value and call it.
-     */
+        Write a program in C# Sharp to show what happen when a struct and a class instance is passed to a method.     */
 
     // crate a class
-    internal class aNewClass
+    internal class newClass
     {
-        public int x;
-        public int y;
+        public int n;
     }
 
-    //create a structure
-    internal struct aNewStruc
+    internal struct newStruct
     {
-        public int x;
-        public int y;
+        public int n;
     }
 
     public class Program
     {
+        private static void trackStruct(newStruct st)
+        {
+            st.n = 1;
+        }
+
+        private static void trackClass(newClass cl)
+        {
+            cl.n = 1;
+        }
+
         private static void Main(string[] args)
         {
-            Console.Write("\n\nCreate a structure and Assign the Value and call it :\n");
-            Console.Write("---------------------------------------------------------\n");
-            aNewClass ClassNum1 = new aNewClass();
-            ClassNum1.x = 1;
-            ClassNum1.y = 2;
-            //	ClassNum2 is ClassNum1 type
-            aNewClass ClassNum2 = ClassNum1;
-            ClassNum1.x = 3;
-            ClassNum1.y = 4;
-            Console.WriteLine("\nAssign in Class:       x:{0},   y:{1}", ClassNum2.x, ClassNum2.y);
-            aNewStruc StrucNum1 = new aNewStruc();
-            StrucNum1.x = 1;
-            StrucNum1.y = 2;
-            //	StrucNum2 is StrucNum1 type
-            aNewStruc StrucNum2 = StrucNum1;
-            StrucNum1.x = 3;
-            StrucNum1.y = 4;
-            Console.WriteLine("Assign in Structure:   x:{0},    y:{1}\n\n", StrucNum2.x, StrucNum2.y);
+            Console.Write("\n\nWhen a struct and a class instance is passed to a method :\n");
+            Console.Write("--------------------------------------------------------------\n");
+            newStruct ns = new newStruct();
+            newClass nc = new newClass();
+            ns.n = 5;
+            nc.n = 5;
+            //value of the struct field did not changed by passing its instance
+            //because a copy of the struct was passed to the trackStruct method
+            trackStruct(ns);
+            //value of the class field changed by passing its instance
+            //because a reference to the class was passed to the tracClass method
+            trackClass(nc);
+            Console.WriteLine("\nns.n = {0}", ns.n);
+            Console.WriteLine("nc.n = {0}\n", nc.n);
             Console.ReadKey();
         }
     }
