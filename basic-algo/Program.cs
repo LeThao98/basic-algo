501260bad00a68fa42b1c15cf2aa293f274de50a
LeThao98 <ledinhthao131098@gmail.com>
2019-11-16 15:00:35 +0700

    structure 02

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -8,27 +8,26 @@ namespace basic_algo
 {
     /*
         structure
-        Write a program in C# Sharp to declare a simple structure.
+        Write a program in C# Sharp to declare a simple structure and use of static fields inside a struct.
     */
 
     internal struct w3rStruct
     {
-        public int x;
-        public int y;
-        private int z;
+        public static int x = 15;
+        public static int y = 25;
+        public int z;
     }
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.Write("\n\nDeclare a simple structure :\n");
+            Console.Write("\n\nStructure with the use of static fields inside a struct :\n");
             Console.Write("--------------------------------\n");
-            w3rStruct w3st = new w3rStruct();
-            w3st.x = 15;
-            w3st.y = 25;
-            int sum = w3st.x + w3st.y;
-            Console.WriteLine("The sum of x and y is {0}\n", sum);
+            int sum = w3rStruct.x + w3rStruct.y;
+            w3rStruct w3R = new w3rStruct();
+            w3R.z = sum;
+            Console.WriteLine("The sum of x and y is {0}\n", w3R.z);
             Console.ReadKey();
         }
     }
