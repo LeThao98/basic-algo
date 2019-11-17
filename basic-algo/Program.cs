18e22e70f865da64e05992ba0981c298d5961e65
LeThao98 <ledinhthao131098@gmail.com>
2019-11-17 19:21:29 +0700

    file-handling 01

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,82 +8,29 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-        structure 10
-        Write a program in C# Sharp to implement a method that returns a structure including calling the method and using its value.
+        file-handling 01
+        Write a program in C# Sharp to create a blank file in the disk newly.
      */
-
-    public struct SampStru
-    {
-        private double val;
-
-        public double Value
-        {
-            get
-            {
-                return val;
-            }
-            set
-            {
-                val = value;
-            }
-        }
-
-        public double Read()
-        {
-            return double.Parse(Console.ReadLine());
-        }
-    }
-
-    public struct Square
-    {
-        private SampStru ln;
-        private SampStru ht;
-
-        public SampStru Length
-        {
-            get { return ln; }
-            set { ln = value; }
-        }
-
-        public SampStru Breadth
-        {
-            get { return ht; }
-            set { ht = value; }
-        }
-
-        public void NewSquare()
-        {
-            SampStru rct = new SampStru();
-            Console.WriteLine("\nInput the dimensions of the Square( equal length and breadth ) : ");
-            ln = SqrLength();
-            Console.Write("breadth : ");
-            ht.Value = rct.Read();
-        }
-
-        public SampStru SqrLength()
-        {
-            SampStru rct = new SampStru();
-
-            Console.Write("length : ");
-            rct.Value = rct.Read();
-            return rct;
-        }
-    }
 
     public class Program
     {
         private static void Main()
         {
-            Console.Write("\n\nMethod that returns a structure  :\n");
-            Console.Write("--------------------------------------\n");
-            var Sqre = new Square();
-            Sqre.NewSquare();
-            Console.WriteLine();
-            Console.WriteLine("Perimeter and Area of the square :");
-            Console.WriteLine("Length:    {0}", Sqre.Length.Value);
-            Console.WriteLine("Breadth:    {0}", Sqre.Breadth.Value);
-            Console.WriteLine("Perimeter: {0}", (Sqre.Length.Value + Sqre.Breadth.Value) * 2);
-            Console.WriteLine("Area:      {0}\n", Sqre.Length.Value * Sqre.Breadth.Value);
+            string fileName = @"mytest.txt";
+            try
+            {
+                Console.Write("\n\n Create a file named mytest.txt in the disk:\n");
+                Console.Write("--------------------------------------------------\n");
+                //Create the file
+                using (FileStream fileStream = File.Create(fileName))
+                {
+                    Console.WriteLine(" A file created with name mytest.txt\n\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
             Console.ReadKey();
         }
     }
