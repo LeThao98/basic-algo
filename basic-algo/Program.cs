ae20b4ba66c03518a1e03f14d27b1bd6cb0e687f
LeThao98 <ledinhthao131098@gmail.com>
2019-11-17 11:44:24 +0700

    structure 09

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,33 +7,42 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-        structure 08
-        Write a program in C# Sharp to demonstrates struct initialization without using the new operator.
+        structure 09
+        Write a program in C# Sharp to insert the information of two books.
      */
 
-    internal struct newStruct
+    internal struct book
     {
-        public int m, n;
-
-        public newStruct(int pt1, int pt2)
-        {
-            m = pt1;
-            n = pt2;
-        }
+        public string title, author;
     }
 
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.Write("\n\nStruct initialization without using the new operator :\n");
-            Console.Write("----------------------------------------------------------\n"); newStruct myPoint1 = new newStruct();
-            newStruct myPoint;
-            myPoint.m = 30;
-            myPoint.n = 20;
-            Console.Write("\nnewStruct : ");
-            Console.WriteLine("m = {0}, n = {1}", myPoint.m, myPoint.n);
+            int nobook = 1000;
+            book[] books = new book[nobook];
+            int i, j, n = 1, k = 1;
+            Console.Write("\n\nInsert the information of two books :\n");
+            Console.Write("-----------------------------------------\n");
+            for (j = 0; j <= n; j++)
+            {
+                Console.WriteLine("Information of book {0} :", k);
 
+                Console.Write("Input name of the book : ");
+                books[j].title = Console.ReadLine();
+
+                Console.Write("Input the author : ");
+                books[j].author = Console.ReadLine();
+                k++;
+                Console.WriteLine();
+            }
+
+            for (i = 0; i <= n; i++)
+            {
+                Console.WriteLine("{0}: Title = {1},  Author = {2}", i + 1, books[i].title, books[i].author);
+                Console.WriteLine();
+            }
             Console.WriteLine("\nPress any key to exit."); ;
             Console.ReadKey();
         }
