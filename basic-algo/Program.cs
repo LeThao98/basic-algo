5618d6368d6939795b2e561575374280863c3f15
LeThao98 <ledinhthao131098@gmail.com>
2019-11-17 11:33:10 +0700

    structure 06

--- basic-algo/Program.cs
+++ basic-algo/Program.cs
@@ -7,48 +7,43 @@ using System.Threading.Tasks;
 namespace basic_algo
 {
     /*
-        structure
-        Write a program in C# Sharp to show what happen when a struct and a class instance is passed to a method.     */
-
-    // crate a class
-    internal class newClass
-    {
-        public int n;
-    }
+        structure 06
+        Write a program in C# Sharp to declares a struct with a property, a method, and a private field.
+     */
 
     internal struct newStruct
     {
-        public int n;
+        private int num;
+
+        public int n
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                if (value < 50)
+                    num = value;
+            }
+        }
+
+        public void clsMethod()
+        {
+            Console.WriteLine("\nThe stored value is: {0}\n", num);
+        }
     }
 
     public class Program
     {
-        private static void trackStruct(newStruct st)
-        {
-            st.n = 1;
-        }
-
-        private static void trackClass(newClass cl)
-        {
-            cl.n = 1;
-        }
-
         private static void Main(string[] args)
         {
-            Console.Write("\n\nWhen a struct and a class instance is passed to a method :\n");
-            Console.Write("--------------------------------------------------------------\n");
-            newStruct ns = new newStruct();
-            newClass nc = new newClass();
-            ns.n = 5;
-            nc.n = 5;
-            //value of the struct field did not changed by passing its instance
-            //because a copy of the struct was passed to the trackStruct method
-            trackStruct(ns);
-            //value of the class field changed by passing its instance
-            //because a reference to the class was passed to the tracClass method
-            trackClass(nc);
-            Console.WriteLine("\nns.n = {0}", ns.n);
-            Console.WriteLine("nc.n = {0}\n", nc.n);
+            Console.Write("\n\nDeclares a struct with a property, amethod, a field:\n");
+            Console.WriteLine("-------------------------------------------------------");
+            newStruct newStruct = new newStruct();
+            newStruct.n = 15;
+            newStruct.clsMethod();
+
             Console.ReadKey();
         }
     }
